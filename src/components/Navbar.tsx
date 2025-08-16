4af4dbd4a11e75d85660067f9c4a0c5bf847a80c
Daniel Modell <dmodell@gmail.com>
2025-08-16 16:15:16 -0400

    Match heart size by using same component and update to exact #ef4445 color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -146,15 +146,13 @@ export function Navbar() {
           </Link>
         ) : (
           <div className="relative flex items-center justify-center">
-            <span className="absolute right-full whitespace-nowrap mr-1.5 text-[10px] md:text-xs lg:text-xs wide:text-sm text-red-600 self-center font-bold">Tip Only</span>
+            <span className="absolute right-full whitespace-nowrap mr-1.5 text-[10px] md:text-xs lg:text-xs wide:text-sm text-[#ef4445] self-center font-bold">Tip Only</span>
             <Link 
               to="../new" 
-              className="flex items-center justify-center h-8 w-8 md:h-8 md:w-8 lg:h-10 lg:w-10 wide:h-10 wide:w-10 focus:outline-none"
+              className="text-[#ef4445] flex items-center justify-center h-8 w-8 md:h-8 md:w-8 lg:h-10 lg:w-10 wide:h-10 wide:w-10 focus:outline-none"
               aria-label="Exit Tip Only"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5 md:h-5 md:w-5 lg:h-6 lg:w-6 wide:h-6 wide:w-6">
-                <path fill="#FF0000" d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-              </svg>
+              <PopiconsHeartDuotone className="h-5 w-5 md:h-5 md:w-5 lg:h-6 lg:w-6 wide:h-6 wide:w-6" />
             </Link>
           </div>
         )}
